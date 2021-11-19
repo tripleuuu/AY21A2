d7017c80f267abdbf8024217f7247c2ae8b65ba8
tripleuuu <xyzuuu@outlook.com>
2021-11-19 00:08:29 +0000

    step2

--- draft.py
+++ draft.py
@@ -1,19 +1,21 @@
 #!/usr/local/bin/python3
-#test species set: Cosmoscarta
+#test protein family set: glucose-6-phosphatase in birds (Aves)
+#This step it to get the file from NCBI using edirect
 
 #to run commands in bash
 import os, subprocess 
 
 #obtain the protein family files and its namelist 
-def importf(beast="Cosmoscarta"):
-beast=input("What's the name of the species?\n")
-getfile1="esearch -db protein -query '{}' | efetch -format fasta > {}.fasta".format(beast,beast)
-getfile2="grep '>' {}.fasta | cut -d ' ' -f -1 > namelist_{}".format(beast,beast)
+#def importf(family="glucose-6-phosphatase in birds (Aves)"):
+#ATTENTION thinking ...
+family=input("What's the name of the protein family?\n")
+getfile="esearch -db protein -query '{}' | efetch -format fasta > '{}.fasta'".format(family,family)
 subprocess.call(getfile1,shell=True)
-sunprocess.call(getfile2,shell=True)
 
-#make files the variable or something
-testvar = str(open("{}.fasta".format(beast)).read().split('>')).split('\\n')
-
-
+#make files the variable or something in python3
+seqlist = open("{}.fasta".format(family)).read().split('>')
+#get the individual sequence
+os.mkdir("alignoutput")	
+	getseq="needle -asequence seqlist -bsequence seqlist -auto -aformat3 pair -outfile ./alignoutput/seq.needle".format(family)
+	subprocess.call(getseq,shell=True)
 
